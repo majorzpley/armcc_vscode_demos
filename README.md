bfb5dffe0474bf30b48c29041c4da2fcd56f6de7
majorzpley <wyx1214844230@outlook.com>
2025-01-24 20:56:52 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # armcc_vscode_demos
 These demos are based on bluepill(stm32f103c8t6_min_dev)
+- 这些演示程序基于蓝色底板（stm32f103c8t6_min_dev）来开发，使用cmake与Ninja来构建工程，使用官方armcc编译器来调试(armclang)未验证，应该问题不大。
+
+## 1、搭建环境
+- 1.1、构建工具**Cmake、Ninja**(**将以上均添加至环境变量path中**)，可以使用Chocolaty来安装或者github下载也行，armcc编译器，MDK官方编译器(**将armcc添加至环境变量path中**)
+- 1.2、vscode插件：**CMake Tools、Cortex-Debug**，可在.vscode文件夹中查看配置文件，我使用的是JLink来调试，所以需要安装JLink驱动(记得将JLink.exe添加至环境变量path中)。
---
+++ led_demo/main.c
@@ -0,0 +1,33 @@
+//? 参考《2.0_STM32F103xx参考手册(英文原版)【重要】》中的page50、112、171
+void delay(int d)
+{
+  while (d--)
+    ;
+}
+int main(void)
+{
+  unsigned int *pReg;
+  unsigned int val;
+  // 使能GPIOC
+  pReg = (unsigned int *)(0x40021000 + 0x18); // RCC_APB2ENR
+  //   val = *pReg;
+  //   *pReg = val | (1 << 4);
+  *pReg |= (1 << 4);
+  // 设置GPIOC13为输出引脚
+  pReg = (unsigned int *)(0x40011000 + 0x04); // GPIOC_CRH
+  *pReg |= (1 << 20);
+  pReg = (unsigned int *)(0x40011000 + 0x0C); // GPIOC_ODR
+  while (1)
+  {
+    // 设置GPIOC13为高电平
+    *pReg |= (1 << 13);
+    // 延时
+    delay(300000);
+    // 设置GPIOC13为低电平
+    *pReg &= ~(1 << 13);
+    // 延时
+    delay(300000);
+  }
+
+  return 0;
+}
