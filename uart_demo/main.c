3b8c519f966366cf7e90f4d907d7212334379127
majorzpley <wyx1214844230@outlook.com>
2025-02-06 19:23:43 +0800

    新增串口的寄存器demo

---
+++ uart_demo/main.c
@@ -0,0 +1,30 @@
+//? 参考《2.0_STM32F103xx参考手册(英文原版)【重要】》中的page50、112、171
+//- 反汇编操作：
+//- fromelf --text -c xxx.elf > xxx.s
+//- 串口的发送与接收
+#include "uart.h"
+void delay(int d) {
+  while (d--)
+    ;
+}
+int main(void) {
+  char c;
+
+  uart_init();
+
+  putchar('1');
+  putchar('0');
+  putchar('0');
+  putchar('a');
+  putchar('s');
+  putchar('k');
+  putchar('\n');
+  putchar('\r');
+
+  while (1) {
+    c = getchar();
+    putchar(c);
+    putchar(c + 1);
+  }
+  return 0;
+}
